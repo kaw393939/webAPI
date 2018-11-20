22214bc760f374be2025f34a0cac15852532f561
lukaszvikas <lzvikas1@gmail.com>
2018-11-20 18:52:44 -0500

    Task: added Vuex configuration

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,11 +4,13 @@ import Vue from "vue";
 import Vuetify from "vuetify";
 import App from "./App.vue";
 import "vuetify/dist/vuetify.min.css";
+import  store from "./store";
 
 //Vue.config.productionTip = false;
 
 Vue.use(Vuetify);
 
 new Vue({
+    store,
     render: h => h(App)
 }).$mount("#app")

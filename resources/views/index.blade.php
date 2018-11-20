9cff7e2710875c4282328021ca425a80c7f8b72e
lukaszvikas <lzvikas1@gmail.com>
2018-11-19 23:30:57 -0500

    Task: added navigation header and some configuration setup

---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token">
+
+    <title>NJIT FAQBot</title>
+
+    <!-- Fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' rel="stylesheet">
+</head>
+<body>
+<div id="app"></div>
+
+<script src="js/app.js"></script>
+</body>
+</html>

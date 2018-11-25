ceec30a45516f89fb9f741db509b88b4ca42fd50
lukaszvikas <lzvikas1@gmail.com>
2018-11-25 12:05:34 -0500

    Fix: removed unecessary code

--- resources/js/store/modules/auth.js
+++ resources/js/store/modules/auth.js
@@ -4,30 +4,17 @@ const state = {
     token: null
 };
 
-const config = {
-    headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "*",
-        "Access-Control-Allow-Methods": ["GET, POST", "PUT", "DELETE"]
-    }
-}
-
-
 const getters = {
     isLoggedIn: state => !!state.token
 };
 
 const actions = {
     signUp: ({commit}, obj) => {
-        console.log("email",obj)
-        console.log("this is signUp")
         axios.post("api/register", obj ).then(res => console.log("RES", res)).catch(err => {
             console.log("ERR", err.response);
         });
     },
     login: ({commit}, obj) => {
-        console.log("email",obj)
-        console.log("this is login")
         axios.post("api/login", obj).then(res => console.log("RES", res)).catch(err => {
             console.log("ERR", err.response);
         });

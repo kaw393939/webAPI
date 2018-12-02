caab0b0554275527435d1abac32326c0d552f330
Tom Goralczyk <goto2141@gmail.com>
2018-12-02 12:11:00 -0500

    Removed unneeded Vuetify's markup

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,19 +1,15 @@
 <template>
-  <v-content style="padding: 0;">
-    <v-container fluid fill-height>
-      <v-layout align-center justify-center>
-        <v-flex xs12 sm8 md4>
-          <div>
-            <h1 class="text">Home</h1>
-          </div>
-        </v-flex>
-      </v-layout>
-    </v-container>
-  </v-content>
+  <v-layout align-center justify-center>
+    <v-flex xs12 sm8 md4>
+      <div>
+        <h1 class="text">Home</h1>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <style>
 .text {
-    text-align: center;
+  text-align: center;
 }
 </style>

896b604bba4640eb3525158211642f2bca382503
shimshamz <31662802+shimshamz@users.noreply.github.com>
2018-12-09 15:21:58 -0500

    Task: Added question_id foreign key field to answers table

--- database/migrations/2018_12_09_003134_create_answers_table.php
+++ database/migrations/2018_12_09_003134_create_answers_table.php
@@ -17,6 +17,8 @@ class CreateAnswersTable extends Migration
             $table->increments('id');
             $table->integer('user_id')->unsigned()->index()->nullable();
             $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('question_id')->unsigned()->index()->nullable();
+            $table->foreign('question_id')->references('id')->on('questions');
             $table->longText('answer');
             $table->timestamps();
         });

dd8e844d3bd33de86cc5ab27a61e088496c671a5
Tom Goralczyk <goto2141@gmail.com>
2018-12-09 10:47:21 -0500

    CardHeader can now accept props for custom styling

--- frontend/src/components/CardHeader.vue
+++ frontend/src/components/CardHeader.vue
@@ -1,5 +1,15 @@
 <template>
-  <v-layout align-center>
+  <v-layout align-center :class="classes">
     <slot></slot>
   </v-layout>
 </template>
+
+<script>
+export default {
+  props: {
+    classes: String,
+    default: ""
+  }
+};
+</script>
+

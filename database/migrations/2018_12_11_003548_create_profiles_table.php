871651d43f8a721f852ba1263eb4e8e327829cd3
shimshamz <31662802+shimshamz@users.noreply.github.com>
2018-12-12 18:12:49 -0500

    Fix: Made user_id field unique

--- database/migrations/2018_12_11_003548_create_profiles_table.php
+++ database/migrations/2018_12_11_003548_create_profiles_table.php
@@ -15,7 +15,7 @@ class CreateProfilesTable extends Migration
     {
         Schema::create('profiles', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id')->nullable();
+            $table->integer('user_id')->unique()->nullable();
             $table->string('first_name')->nullable();
             $table->string('last_name')->nullable();
             $table->string('bio')->nullable();

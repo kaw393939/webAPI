74a61badaa1b943b63e04158505a7b072a4e5bdc
shimshamz <31662802+shimshamz@users.noreply.github.com>
2018-12-12 18:11:18 -0500

    Fix: Updated fillable array

--- app/Profile.php
+++ app/Profile.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
+    protected $fillable = [
+        'first_name', 'last_name', 'bio',
+    ];
+
     public function user() {
         return $this->belongsTo('App\User');
     }

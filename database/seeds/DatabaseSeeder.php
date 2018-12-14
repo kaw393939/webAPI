2b4bb7f2dc18fa2391a1115d44964bbe954e252a
shimshamz <31662802+shimshamz@users.noreply.github.com>
2018-12-13 20:10:05 -0500

    Task: Created separate seeder files for Users, Profiles and Questions

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,16 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\User::class, 50)->create()->each(function ($user) {
-        });
-
-        $users = App\User::all();
-        for ($i = 1; $i <= 16; $i++) {
-            $users->each(function ($user) {
-                $question = factory(\App\Question::class)->make();
-                $question->user()->associate($user);
-                $question->save();
-            });
-        }
+        $this->call(UsersTableSeeder::class);
+        $this->call(ProfilesTableSeeder::class);
+        $this->call(QuestionsTableSeeder::class);
     }
 }

3f4174788a908ced4bc46bd521ac7ab22519e6e5
Tom Goralczyk <goto2141@gmail.com>
2018-12-16 14:58:04 -0500

    Removed unnecessary code

--- frontend/src/store/modules/auth.js
+++ frontend/src/store/modules/auth.js
@@ -1,43 +1,13 @@
-import router from "../../router";
-import { setToken, getToken } from "../../utilities/localStorage";
-import { setAuthToken } from "@/utils/LocalStorageUtils";
-
 const state = {
-    token: getToken(),
-    error: "",
-    user: "",
     authUser: {},
     isLogged: false
 };
 
 const getters = {
-    isLoggedIn: state => state.isLogged,
-    error: state => state.error,
-    userData: state => state.user
-};
-
-const actions = {
-    clearErrors: ({ commit }) => {
-        console.log("clear");
-        commit("removeErrors");
-    }
+    isLoggedIn: state => state.isLogged
 };
 
 const mutations = {
-    sendError: (state, error) => {
-        state.error = error;
-    },
-    sendUserData: (state, userData) => {
-        userData ? (state.user = userData) : (state.user = "");
-    },
-    setLoggedState(state, authToken) {
-        if (authToken) state.isLogged = true;
-        else state.isLogged = false;
-    },
-    removeErrors: state => {
-        state.error = "";
-    },
-
     setAuthUser(state, payload) {
         state.authUser = { ...payload.data };
         state.isLogged = true;
@@ -52,6 +22,5 @@ const mutations = {
 export default {
     state,
     getters,
-    actions,
     mutations
 };

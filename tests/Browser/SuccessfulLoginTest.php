77f3f8ba2809b1ee73f9d7a9ad21b112d0bcc376
kjp44 <Sabbath51>
2018-12-18 21:03:39 -0500

    Task: Changed test to wipe database, seed it with a specific user, and log in with that specific user.

--- tests/Browser/SuccessfulLoginTest.php
+++ tests/Browser/SuccessfulLoginTest.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace Tests\Browser;
+namespace App;
 
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use App\User;
 
 class SuccessfulLoginTest extends DuskTestCase
 {
@@ -15,12 +17,17 @@ class SuccessfulLoginTest extends DuskTestCase
      */
     public function testSuccessfulLogin()
     {
+        $this->artisan('migrate:refresh');
         $this->browse(function (Browser $browser) {
+            $user = new User;
+            $user->email = 'test@example.com';
+            $user->password = bcrypt('your_password');
+            $user->save();
             $browser->visit('/')
                     ->clickLink('Login')
                     ->assertPathIs('/login')
-                    ->type('email', 'kjp44@njit.edu')
-                    ->type('password', 'secret')
+                    ->type('email', $user->email)
+                    ->type('password', 'your_password')
                     ->press('LOGIN')
                     ->assertPathIs('/');
         });
